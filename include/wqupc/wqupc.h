a2203f3d52434b49639a683b1d999c578f027354
Jitsu-Net <jitsu-net@xaepient.com>
2017-02-04 10:43:23 -0500

    Initial autotools scaffolding

---
+++ include/wqupc/wqupc.h
@@ -0,0 +1,29 @@
+#ifndef ORG_XAEPIENT_WQUPC_H_
+#define ORG_XAEPIENT_WQUPC_H_
+
+#include <vector>
+
+namespace org {
+namespace xaepient {
+namespace wqupc {
+
+class WQUPC
+{
+public:
+    WQUPC();
+    WQUPC(std::size_t size);
+    ~WQUPC() = default;
+    bool connected(std::size_t p, std::size_t q);
+    void join(std::size_t p, std::size_t q);
+private:
+    std::size_t root(std::size_t i);
+    std::vector<std::size_t> id;
+    std::vector<std::size_t> sz;
+}; // class WQUPC
+
+} // namespace wqupc
+} // namespace xaepient
+} // namespace org
+
+#endif
+
